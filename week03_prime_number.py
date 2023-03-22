3a5169091b66d9318d979f8b3ce13db4849fdb54
inhawinter <inhawinter@gmail.com>
2023-03-22 21:28:07 +0900

    week03 prime number quiz

--- week03_prime_number.py
+++ week03_prime_number.py
@@ -1,16 +1,18 @@
-# week03_prime_number v0.6
-number = int(input("Input number : "))
+# week03_prime_number quiz
+start_no = int(input("Enter starting number : "))
+end_no = int(input("Enter ending number : "))
 is_prime = True
 
-if number < 2:
-    is_prime = False
-else:
-    for i in range(2, number):
-        if number % i == 0:
-            is_prime = False
-            break
+for k in range(start_no, end_no+1):
+    is_prime = True
+    if k < 2:
+        is_prime = False
+    else:
+        for i in range(2, k):
+            if k % i == 0:
+                is_prime = False
+                break
+        if is_prime:
+            print(k, end=' ')
 
-if is_prime:
-    print(f'{number} is prime number!')
-else:
-    print(f'{number} is NOT prime number.')
+
