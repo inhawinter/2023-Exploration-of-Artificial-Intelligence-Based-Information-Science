8a98ef969ed26d24b4fefc37cdfbec1676f57a1e
ag4710 <leun4710@gmail.com>
2023-03-29 20:59:27 +0900

    module prime number v1.1

---
+++ eunhwanlee/prime_number.py
@@ -0,0 +1,26 @@
+# week04 prime_number v1.1
+# add is_prime function
+# Lee EunHwan
+
+def is_prime(n) -> bool:
+    """
+    Check prime number
+    :param n: integer number
+    :return: True (if number is prime number) / False (if number is NOT prime number)
+    """
+    if k < 2:
+        return False
+    else:
+        for i in range(2, k):
+            if k % i == 0:
+                return False
+    return True
+
+start_no, end_no = map(int, input("Enter starting number and ending number : ").split())
+
+if start_no > end_no:
+    start_no, end_no = end_no, start_no
+
+for k in range(start_no, end_no+1):
+    if is_prime(k):
+        print(k, end=' ')
