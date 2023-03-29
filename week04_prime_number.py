9a855090a51f08bb31ca11d96ebe6b8d3ce8480f
0917ba <0917ba@naver.com>
2023-03-29 20:10:56 +0900

    week04 prime number v1.1

--- week04_prime_number.py
+++ week04_prime_number.py
@@ -1,5 +1,6 @@
 # week04 prime_number v1.0
 # add is_prime function
+# Lee woojin
 
 def is_prime(n) -> bool:
     """
@@ -7,17 +8,30 @@ def is_prime(n) -> bool:
     :param n: integer number
     :return: True (if number is prime number) / False (if number is NOT prime number)
     """
-    if k < 2:
+    
+    if n < 2:
         return False
     else:
-        for i in range(2, k):
-            if k % i == 0:
+        for i in range(2, n):
+            if n % i == 0:
                 return False
     return True
 
 
+def reversable_prime_print(low_num, high_num):
+
+    # swap lowNum & highNum
+    if low_num > high_num:
+        temp_num = low_num
+        low_num = high_num
+        high_num = temp_num
+
+    # printing primes
+    for k in range(low_num, high_num + 1):
+        if is_prime(k):
+            print(k, end=' ')
+
+    
 start_no, end_no = map(int, input("Enter starting number and ending number : ").split())
 
-for k in range(start_no, end_no+1):
-    if is_prime(k):
-        print(k, end=' ')
+reversable_prime_print(start_no, end_no)
