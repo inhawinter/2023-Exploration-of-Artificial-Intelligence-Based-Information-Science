d2c3a2638383580261f1f2b133a81c39ae15f8be
inhawinter <inhawinter@gmail.com>
2023-03-29 21:22:47 +0900

    week04 copy v0.3

--- week04_copy.py
+++ week04_copy.py
@@ -1,9 +1,7 @@
-a = [-9, 77, 100]
+a = [-9, [77, 13], 100]
 b = a[:]  # copy
 c = list(a)  # copy
 d = a.copy()  # copy
 print(id(a), id(b), id(c), id(d))
-b[1] = 16
-c[1] = 0
-d[1] = 55
+b[1][0] = 16
 print(a, b, c, d)
