b6dc9f318621f0d1d3a521e1d8b4b6e830a50f47
kanghwimun <kanghm0602@gmail.com>
2023-05-24 19:22:24 +0900

    week10 class&object 05

--- week09_class_object03.py
+++ week09_class_object03.py
@@ -1,16 +1,50 @@
 class Pokemon:
+    def __init__(self, name, level, hp):
+        self.name = name
+        self.level = level
+        self.hp = hp
+
+    def say(self):
+        print(f"I'm a pokemon. My name is {self.name}")
+
+
+    def attack(self):
+        print(f"{self.name} launches an area attack.")
+
+
+class Pikachu(Pokemon):  # is-a relationship, inheritance
+    def attack(self):
+        print(f"{self.name} launches an area-of-effect electric attack.")
+
+
+class Squirtle(Pokemon):
+    def attack(self):
+        print(f"{self.name} casts a wide-area water cannon attack.")
+
+
+class Charizard(Pokemon):
+    def attack(self):  # derived class method, override
+        super().attack()  # base class method
+        print(f"{self.name} casts a wide-area fire attack.")
+
+
+class Digimon:
     pass
 
 
 if __name__ == "__main__":
-    pikachu = Pokemon() # instance creation
-    eevee = Pokemon() # instance creation
+    #pikachu.say()
+    pikachu = Pikachu("pikachu", 1, 35)
+    pikachu.say()
+    squirtle = Squirtle("squirtle", 1, 44)
+    charizard = Charizard("charizard", 36, 78)
+    charizard.say()
 
-    pikachu.level = 1
-    pikachu.hp = 35
-    pikachu.nemesis = eevee
+    charizard.attack()
+    pikachu.attack()
+    squirtle.attack()
 
-    print(pikachu.hp)
-    print(pikachu.nemesis)
-    print(pikachu)
-    print(eevee.hp)
+    print(squirtle.name)
+    print(pikachu.name)
+    print(issubclass(Squirtle, Pokemon))
+    print(issubclass(Digimon, Pokemon))
