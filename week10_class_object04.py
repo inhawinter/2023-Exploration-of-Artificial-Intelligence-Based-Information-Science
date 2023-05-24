587c0364a2a49f7cd63f345b27b2cdb70bec5efd
kanghwimun <kanghm0602@gmail.com>
2023-05-24 20:08:00 +0900

    week10 class&object 07

--- week10_class_object04.py
+++ week10_class_object04.py
@@ -2,28 +2,44 @@
 
 class PrettyMixin:
     def dump(self):
-        print(f"{self.name} pokemon")
+        # print(f'{self.name} pokemon')
         import pprint
         pprint.pprint(vars(self))
 
 
 class Pokemon(PrettyMixin):
-    def __init__(self, name, hp, level):
-        self.name = name
+    def __init__(self, input_name, hp, level):
+        self.hidden_name = input_name
         self.hp = hp
         self.level = level
 
+    @property
+    def name(self):
+        print("getter executed!")
+        return self.hidden_name
+
+    @name.setter
+    def name(self, input_name):
+        print("setter executed!")
+        self.hidden_name = input_name
+
+
     def info(self):
-        print("============")
-        print(f"Name : {self.name}")
+        print("================")
+        print(f"Name : {self.hidden_name}")
         print(f"Hp : {self.hp}")
-        print(f"level : {self.level}")
-        print("============")
+        print(f"Level : {self.level}")
+        print("================")
 
 
 if __name__ == "__main__":
     p1 = Pokemon("pikachu", 35, 1)
     p2 = Pokemon("squirtle", 40, 1)
-    p2.info()
+    p2.info()  # Pokemon.info(p2)
     p1.dump()
     p2.dump()
+    p2.level = 2  # direct access
+    p2.info()
+    p2.name = "wartortle"
+    print(p2.name)
+
