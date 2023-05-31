065be5c50d2044f5ee0ebfa7e1c2f3f0407d66ad
kanghwimun <kanghm0602@gmail.com>
2023-05-31 20:45:48 +0900

    week11 numpy 06

--- week11_numpy.py
+++ week11_numpy.py
@@ -1,13 +1,9 @@
 import numpy as np
 
-d2_array = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-# list style
-print(d2_array[2][0], d2_array[-1][-3], d2_array[2][-3])
-# numpy style
-print(d2_array[2, -3], d2_array[-1, 0])
-
-d2_array[0, 0] = 11
-print(d2_array)
-
-d2_array[0, 0] = 22.71  # truncation
-print(d2_array)
+d2_33 = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+print(d2_33 > 5)
+print(d2_33[d2_33 > 5])
+print(d2_33[:, 2])
+print(d2_33[:, 2] > 5)
+print(d2_33[:] % 2 == 0)
+print(d2_33[d2_33[:] % 2 == 0])
