14b03b602ea6c72c4742ad851f0bc589164d8928
kanghwimun <kanghm0602@gmail.com>
2023-05-31 20:10:02 +0900

    week11 numpy 04

--- week11_numpy.py
+++ week11_numpy.py
@@ -1,13 +1,15 @@
 import numpy as np
 
-#v = np.array([90, 70.0, "BTS", 80])
-#m = np.array([[3, 2, 1], [7, 8, 9], [10, 12, 11]])
-v = np.array([90, 70, 80])
-m = np.array([[3, 2, 1], [7, 8, 9], [10, 12, 11]])
-print(v, m)
-print(v.ndim, m.ndim)
-print(v.shape, m.shape)
-print(v.dtype, m.dtype)
-print(v.itemsize, m.itemsize)
-print(v.nbytes, m.nbytes)
-print(v.strides,m.strides)
+# b = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]) + np.array([10, 20, 30, 40, 50, 60, 70, 80, 90, 100])
+# b = np.array(range(1, 11)) + np.array(range(10, 101, 10))
+b = np.arange(1, 11) + np.arange(10, 101, 10)
+print(b, b.shape, b.size, b.ndim)
+
+scores = np.array([2500000, 2200000, 2300000])
+# scores = scores + 50
+# scores = scores * 3
+scores = scores * 0.026 / 12.0
+print(scores)
+
+mixed = np.array([-9, 'ISHS', False, 2.71])
+print(mixed)
