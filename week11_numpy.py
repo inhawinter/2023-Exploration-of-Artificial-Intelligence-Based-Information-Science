11a1429faa5bbd8e82884bde0065d48852885db5
inhawinter <inhawinter@gmail.com>
2023-05-31 19:41:57 +0900

    week11 numpy 03

--- week11_numpy.py
+++ week11_numpy.py
@@ -1,23 +1,13 @@
 import numpy as np
 
-# python list
-mid = [10, 20, 30]
-final = [90, 70, 80]
-
-# numpy array
-mid_scores = np.array(mid)
-final_scores = np.array(final)
-
-print(type(mid), type(mid_scores))
-
-total_np = mid_scores + final_scores
-print(f"Total : {total_np}")
-print(f"Average : {total_np/2}")
-
-# total = list()
-# avg = []
-# for i in range(len(mid)):
-#     total.append(mid[i] + final[i])
-#     avg.append(total[i]/2)
-# print(f"Total : {total}")
-# print(f"Average : {avg}")
+# v = np.array([90, 70.0, "BTS", 80])
+# v = np.array([90, 70.0, 80])
+v = np.array([90, 70, 80])
+m = np.array([[3, 2, 1], [7, 8, 9], [10, 12, 11]])
+print(v, m)
+print(v.ndim, m.ndim)
+print(v.shape, m.shape)
+print(v.dtype, m.dtype)
+print(v.itemsize, m.itemsize)
+print(v.nbytes, m.nbytes)
+print(v.strides, m.strides)
