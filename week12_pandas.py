ab2931d724ffa7cc99b6ee24bba089d4ce5fe19a
inhawinter <inhawinter@gmail.com>
2023-06-14 20:21:30 +0900

    week12 pandas 07

--- week12_pandas.py
+++ week12_pandas.py
@@ -3,21 +3,16 @@ import pandas as pd
 data1 = {
     "a": [11, -2, 7],
     "b": [9, 0, 77],
-    "c": [55, 33, 19]
+    "c": [7, 33, 19]
 }
 data2 = [
-            [11, 9, 55],
+            [11, 9, 7],
             [-2, 0, 33],
             [7, 77, 19]
 ]
 df1 = pd.DataFrame(data1, index=[1, 2, 3])
 df2 = pd.DataFrame(data2, index=[1, 2, 3], columns=["a", "b", "c"])
 
-print(df2.iat[1, 0])
-print(df2.iloc[1:3, 1:3])
-print(df2.loc[2:3, 'b':'c'])
-
-print(df1)
 df3 = df1\
     .melt()\
     .rename(columns={
@@ -25,10 +20,10 @@ df3 = df1\
         'value': 'val'
     })\
     .sort_values('val', ascending=False)\
-    .tail(6)\
-    .query('val > 10')
 
 print(df3)
-print(df1.drop(columns=['b']))
-print(df1)
-
+print(df3.describe())
+# print(df3['val'].value_counts())
+# print(df3.shape)
+# print(df3['val'].nunique())
+print(df2.describe())
