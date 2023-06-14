d6b7040945bbf3ec027e20c92c5d1e008e1c13d4
kanghwimun <kanghm0602@gmail.com>
2023-06-14 19:27:30 +0900

    week12 pandas 03

--- week12_pandas.py
+++ week12_pandas.py
@@ -1,18 +1,19 @@
 import pandas as pd
 
-df1 = pd.DataFrame({
+data1 = {
     "a": [11, -2, 7],
     "b": [9, 0, 77],
     "c": [55, 33, 19]
-     }, index=[1, 2, 3])
+}
 
-df2 = pd.DataFrame(
-    [[11, 9, 55],
-     [-2, 0, 33],
-     [7, 77, 19]],
-    index=[1, 2, 3],
-    columns=["a", "b", "c"]
-)
+data2 = [
+            [11, 9, 55],
+            [-2, 0, 33],
+            [7, 77, 19]
+]
+df1 = pd.DataFrame(data1, index=[1, 2, 3])
+
+df2 = pd.DataFrame(data2, index=[1, 2, 3], columns=["a", "b", "c"])
 
 print(df1)
 print(df2)
