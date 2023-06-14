9c20b1d3bad5235b53933863d30db91deb974157
inhawinter <inhawinter@gmail.com>
2023-06-14 20:11:12 +0900

    week12 pandas 06

--- week12_pandas.py
+++ week12_pandas.py
@@ -12,6 +12,11 @@ data2 = [
 ]
 df1 = pd.DataFrame(data1, index=[1, 2, 3])
 df2 = pd.DataFrame(data2, index=[1, 2, 3], columns=["a", "b", "c"])
+
+print(df2.iat[1, 0])
+print(df2.iloc[1:3, 1:3])
+print(df2.loc[2:3, 'b':'c'])
+
 print(df1)
 df3 = df1\
     .melt()\
@@ -20,6 +25,7 @@ df3 = df1\
         'value': 'val'
     })\
     .sort_values('val', ascending=False)\
+    .tail(6)\
     .query('val > 10')
 
 print(df3)
